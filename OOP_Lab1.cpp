99e3e0f9a7f9f005d478a6bce9013cfdeb79f792
Shein Denis <ja1lbr3ak@yandex.ru>
2025-03-23 13:12:44 +0300

    added text menu

--- OOP_Lab1.cpp
+++ OOP_Lab1.cpp
@@ -4,26 +4,84 @@
 using namespace std;
 
 int PPM = 10; //Pixels Per Move
+bool EXIT_TASK = false; //to break infinite loops
 
 int main() {
+    setlocale(LC_ALL, "RUSSIAN");
     HWND hWnd = FindWindowA("ConsoleWindowClass", NULL);
     HDC hdc = GetDC(hWnd);
     HBRUSH hBrush = CreateSolidBrush(RGB(255, 255, 255));
     SelectObject(hdc, hBrush);
     int x = 600, y = 600; // Измените координаты
-    Fish AFish(x, y);
+    //Fish AFish(x, y);
     PatrioticFish BFish(400, 400);
     Rectangle(hdc, 0, 0, 1920, 1080);
 
     while(1)
     {
-        cout << "Выберите действие:";
-        /*AFish.Show();
-        BFish.Show();
-        system("pause");
-        BFish.patrioticForm();
-        BFish.drag(PPM);*/
-        //AFish.drag(PPM);
+        EXIT_TASK = false;
+        cout << "Выберите действие: \n"
+            << "1 - Создать базовую рыбу\n"
+            << "2 - Создать рыбу - патриота\n";
+        int choice;
+        cin >> choice;
+        switch (choice) {
+        case 1: {
+            cout << "Введите координаты (x,y): \n";
+            cin >> x >> y;
+            Fish AFish(x, y);
+            while (!EXIT_TASK) {
+                cout << "Выберите действие над базовой рыбой: \n"
+                    << "1 - Показать на экране\n"
+                    << "2 - Скрыть\n"
+                    << "3 - Перемещать\n"
+                    << "Другая цифра - Вернуться на главную\n";
+                cin >> choice;
+                switch (choice) {
+                case 1: { AFish.Show();    break; }
+                case 2: { AFish.Hide();    break; }
+                case 3: { AFish.drag(PPM); break; }
+                default: {
+                    EXIT_TASK = true;
+                    cout << "Возращаем...\n";
+                    break;
+                }
+                }
+            }
+            break;
+        }
+        
+            case 2: {
+                cout << "Введите координаты (x,y): \n";
+                cin >> x >> y;
+                PatrioticFish BFish(x, y);
+                while (!EXIT_TASK) {
+                    cout << "Выберите действие над базовой рыбой: \n"
+                        << "1 - Показать на экране\n"
+                        << "2 - Скрыть\n"
+                        << "3 - Перемещать\n"
+                        << "4 - Патриотичная форма\n"
+                        << "5 - Вернуться в обычную форму\n"
+                        << "Другая цифра - Вернуться на главную\n";
+                    cin >> choice;
+                    switch (choice) {
+                    case 1: { BFish.Show();    break; }
+                    case 2: { BFish.Hide();    break; }
+                    case 3: { BFish.drag(PPM); break; }
+                    case 4: { BFish.patrioticForm(); break; }
+                    case 5: { BFish.baseForm(); break; }
+                    default: {
+                        EXIT_TASK = true;
+                        cout << "Возращаем...\n";
+                        break;
+                    }
+                    }
+                }
+                break;
+            
+            default:
+                break;
+        }
     }
 
     ReleaseDC(hWnd, hdc); // Освободите контекст устройства
